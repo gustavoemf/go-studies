8732d30f36ba269ba6fa961c201ecf6fa3268a3b
gustavo.muller <gustavoeduardomf@gmail.com>
2025-12-18 16:59:43 -0300

    exercícios nível 2, operadores condicionais e decimal, binário e hexadecimal

---
+++ exercises/n2/06_ninja/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Escreva um programa que mostre um número em decimal, binário, e hexadecimal.
+	x := 10
+	fmt.Printf("%d, %#x, %b", x, x, x)
+}
---
+++ exercises/n2/07_ninja/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 	Escreva expressões utilizando os seguintes operadores, e atribua seus valores a variáveis.
+	// ==
+	// !=
+	// <=
+	// <
+	// =
+
+	// Demonstre estes valores.
+	comparacao := (1 == 0)
+	diferenca := (1 != 0)
+	menorigual := (1 <= 0)
+	menor := (1 < 0)
+
+	fmt.Printf("%v, %v , %v, %v", comparacao, diferenca, menorigual, menor)
+}
