be2e9f098f344b0f6a2c6dcaab328bc150c42f28
gustavo.muller <gustavoeduardomf@gmail.com>
2025-12-18 17:56:36 -0300

    exercicio nivel 2, raw string literal

---
+++ exercises/n2/10_ninja/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	Crie uma variável de tipo string utilizando uma raw string literal.
+	//
+	// Demonstre-a.
+
+	x := `Isso é um raw string literal
+            Printa literal tudo
+
+		feito com a crase e não aspas`
+	fmt.Println(x)
+}
